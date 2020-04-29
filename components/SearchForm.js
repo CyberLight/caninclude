129e686e2c3512810df9686b1aa61c6b26e8591b
CyberLight <cyberlight@live.ru>
2020-04-29 11:04:42 +0000

    SearchForm: Added show prop and disable autocomplete
    * Added link for "Can I Include" label

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -1,14 +1,14 @@
 const { html, Component } = require('htm/preact');
 
 class SearchForm extends Component {
-    render({ parent = '', child = '' }) {
-        return html`
+    render({ parent = '', child = '', show = true }) {
+        return show && html`
         <section class="search">
-            <form id="search" class="search__form" action="/can/include/" method="get">
-                <label class="head" for="child">Can I include</label>
-                <input type="text" id="child" name="child" placeholder="Parent tag name" value="${child}"/>
+            <form id="search" autocomplete="off" class="search__form" action="/can/include/" method="get">
+                <label class="head" for="child"><a class="head__link--regular" href="/">Can I include</a></label>
+                <input type="search" id="child" name="child" autocomplete="off" placeholder="Child tag name" value="${child}"/>
                 <span class="divider">to</span>
-                <input type="text" id="parent" name="parent" placeholder="Child tag name" value="${parent}"/>
+                <input type="search" id="parent" name="parent" autocomplete="off" placeholder="Parent tag name" value="${parent}"/>
                 <button class="search__button" type="submit">?</button>
             </form>
         </section>`;
