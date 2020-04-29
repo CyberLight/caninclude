0bc29c699acf021056e3cc327fb58d33ebacbb13
CyberLight <cyberlight@live.ru>
2020-04-29 11:02:28 +0000

    Cleanup Support component from reduceData method
    * This method was moved to crawl script

--- components/Support.js
+++ components/Support.js
@@ -1,36 +1,7 @@
 const { html, Component } = require('htm/preact');
 
 class Support extends Component {   
-    reduceData(data) {
-        const sites = ['WebHTMLElement', 'WebAPI', 'caniuse'];
-        const browsers = sites.reduce((browsers, site) => {
-            if (!data[site]) return browsers;
-            const siteBrowsers = data[site].support.browsers;
-            for (let [browser,] of siteBrowsers) {
-                browsers.add(browser)
-            }
-            return browsers;
-        }, new Set());
-
-        const table = {};
-        const cache = {};
-        for (const site of sites) {
-            for (const browser of browsers) {
-                let supportBrowsers;
-                if (data[site]) {
-                    supportBrowsers = cache[site] || Object.fromEntries(data[site].support.browsers);
-                    if (!cache[site]) cache[site] = supportBrowsers;
-                }
-                table[browser] = table[browser] || {}; 
-                table[browser][site] = table[browser][site] || {};
-                table[browser][site] = data[site] && supportBrowsers[browser] || '--';
-            }
-        }
-        return table;
-    }
-
     render({ data }) {
-        const reducedData = this.reduceData(data);
         return html`
             <table class="tag__table">
                 <tr class="table__head">
@@ -39,8 +10,8 @@ class Support extends Component {
                     <th>Web API</th>
                     <th>CanIUse</th>
                 </tr>
-                ${Object.entries(reducedData).map(([browser, row]) => 
-                    html`<tr><td>${browser}</td>${Object.values(row)
+                ${Object.entries(data).map(([browser, row]) => 
+                    html`<tr class="table__tr"><td>${browser}</td>${Object.values(row)
                             .map(cell => html`<td>${cell}</td>`)}</tr>`)}  
             </table>
         `;
