12154bbac0549c66f770b3d759a5d50b1be8c7dd
CyberLight <cyberlight@live.ru>
2020-04-29 14:36:30 +0000

    Added pack script for glitch release

---
+++ scripts/glitch.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const archiver = require('archiver');
+const output = fs.createWriteStream(`glitch_release_${+new Date()}.zip`);
+const archive = archiver('zip', {
+    zlib: { level: 9 } // Sets the compression level.
+});
+
+output.on('close', () => {
+    // eslint-disable-next-line no-console
+    console.log(`${archive.pointer()} total bytes`);
+    // eslint-disable-next-line no-console
+    console.log('archiver has been finalized and the output file descriptor has closed.');
+});
+
+output.on('end', () => {
+    // eslint-disable-next-line no-console
+    console.log('Data has been drained');
+});
+
+archive.on('warning', (err) => {
+    if (err.code === 'ENOENT') {
+        // log warning
+        // eslint-disable-next-line no-console
+        console.warn(err);
+    } else {
+        // throw error
+        throw err;
+    }
+});
+
+archive.on('error', (err) => {
+    throw err;
+});
+
+archive.pipe(output);
+
+archive.directory('components/', 'components');
+archive.file('package-lock.json', { name: 'package-lock.json' });
+archive.file('package.json', { name: 'package.json' });
+archive.file('server.js', { name: 'server.js' });
+archive.file('spec.json', { name: 'spec.json' });
+
+archive.finalize();
