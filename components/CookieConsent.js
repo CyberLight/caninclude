779814f2518e145b0a7e6e8b9dd7450a9dc2c83f
CyberLight <cyberlight@live.ru>
2020-05-05 14:38:58 +0000

    Added CookieConsent component for accepting usage of cookies

---
+++ components/CookieConsent.js
@@ -0,0 +1,17 @@
+const { html, Component } = require('htm/preact');
+
+class CookieConsent extends Component {
+    render() {
+        return html`
+        <div class="cookie-consent">
+            This site uses cookies to evaluate the quality of the result of determining whether a tag can be included in a tag.
+            <a class="cookie-consent__accept-button" href="/cookies/accept">
+                <span class="accept-button__inner">
+                    Accept
+                </span>
+            </a>
+        </div>`;
+    }
+}
+
+module.exports = CookieConsent;
