54b17b56b6be74806495141dcf1c8dc23ca0845f
CyberLight <cyberlight@live.ru>
2020-05-05 16:25:29 +0000

    Added more clarity to text of CookieConsent component

--- components/CookieConsent.js
+++ components/CookieConsent.js
@@ -4,7 +4,7 @@ class CookieConsent extends Component {
     render() {
         return html`
         <div class="cookie-consent">
-            This site uses cookies to evaluate the quality of the result of determining whether a tag can be included in a tag.
+            This site uses cookies to evaluate the quality of the result of determining whether a tag can be included in a tag. <br/> By accepting the use of cookies, you can vote “like” or “dislike”.
             <a class="cookie-consent__accept-button" href="/cookies/accept">
                 <span class="accept-button__inner">
                     Accept
