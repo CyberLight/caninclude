67764d78a5130cb6c01561cde227bc2348eb9b14
CyberLight <cyberlight@live.ru>
2020-05-08 12:50:07 +0000

    Added Main component which represenf main layout of any page

---
+++ components/Main.js
@@ -0,0 +1,22 @@
+const { html, Component } = require('htm/preact');
+const SearchForm = require('./SearchForm');
+const Footer = require('./Footer');
+
+class Main extends Component {
+    render({ children, form, tags, request }) {
+        const hasTags = tags && tags.length > 0;
+        return html`
+        <div class="app">
+            <header class="header">
+                <${SearchForm} ...${form} show="${hasTags}"/>
+                <a class="header__marker" href="#">
+                    <div class="marker__content">Alpha<br/>version</div>
+                </a>
+            </header>
+            ${children}
+            <${Footer} count="${request && request.count || '0'}" uniqCount="${request && request.uniqCount || '0'}"/>
+        </div>`;
+    }
+}
+
+module.exports = Main;
