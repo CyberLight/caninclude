6eff12a874dd99424d853b10b4365b68a3112e52
CyberLight <cyberlight@live.ru>
2020-05-08 12:51:22 +0000

    Move shared part of App layout to Main component

--- components/App.js
+++ components/App.js
@@ -9,40 +9,38 @@ const Tips = require('./Tips');
 const About = require('./About');
 const QuickResults = require('./QuickResults');
 const CookieConsent = require('./CookieConsent');
+const NewFeedback = require('./NewFeedback');
+const TagFeedbacks = require('./TagFeedbacks');
+const Main = require('./Main');
 
 class App extends Component {
-    render({ form, tags, tips = [], tagStats, request, specVersion, votes, userAcceptCookie }) {
+    render({ form, tags, tips = [], tagStats, request, specVersion, votes, userAcceptCookie, showFeedback, showFeedbacks, feedback, feedbacks }) {
         const hasTags = tags.length > 0;
 
         return html`
-        <div class="app">
-            <header class="header">
-                <${SearchForm} ...${form} show="${hasTags}"/>
-                <a class="header__marker" href="#">
-                    <div class="marker__content">Alpha<br/>version</div>
-                </a>
-            </header>
-            ${ !userAcceptCookie && html`<${CookieConsent}/>` }
-            <main class="content">
-                <${Tips} tips="${tips}"/>
-                <${Tags} show="${hasTags}">
-                    <input checked id="first" class="tab__option" type="radio" name="tabs" value="first"/>
-                    <label class="tab__name" for="first">${tags.length && tags[0].tags.list.map(item => `<${item}/>`).join(', ')}</label>
-                    <input id="second" class="tab__option" type="radio" name="tabs" value="second"/>
-                    <label class="tab__name" for="second">${tags.length && tags[2].tags.list.map(item => `<${item}/>`).join(', ')}</label>
-                    <${Section} tag="${tags[0]}" accent="first"/>
-                    <${ResultSection} ..."${tags[1]}" request="${request}" votes="${votes}" userAcceptCookie="${userAcceptCookie}"/>
-                    <${Section} tag="${tags[2]}" accent="last"/>
-                </${Tags}>
-                <${About} show="${!hasTags}">
-                    <${MainSearchForm} ...${form} show="${!hasTags}" specVersion="${specVersion}"/>
-                    <${QuickResults} tagStats="${tagStats}"/>
-                    <div class="about__description">This site helps you understand which tag you can include in another using the <a target="_blank" rel="noopener noreferrer" class="link--no-decoration" href="https://html.spec.whatwg.org"> WHATWG HTML specification </a></div>
-                    <div class="about__description">* This is an alpha version and uses a simple algorithm to test whether one tag can be included in another.</div>
-                </${About}>
-            </main>
-            <${Footer} count="${request.count}" uniqCount="${request.uniqCount}"/>
-        </div>`;
+            <${Main} form="${form}" tags="${tags}" request="${request}">
+                ${ !userAcceptCookie && html`<${CookieConsent}/>` }
+                <main class="content">
+                    <${Tips} tips="${tips}"/>
+                    <${NewFeedback} request="${request}" form="${form}" show="${showFeedback}"/>
+                    <${TagFeedbacks} request="${request}" feedbacks="${feedbacks}" show="${showFeedbacks}" />
+                    <${Tags} show="${hasTags}">
+                        <input checked id="first" class="tab__option" type="radio" name="tabs" value="first"/>
+                        <label class="tab__name" for="first">${tags.length && tags[0].tags.list.map(item => `<${item}/>`).join(', ')}</label>
+                        <input id="second" class="tab__option" type="radio" name="tabs" value="second"/>
+                        <label class="tab__name" for="second">${tags.length && tags[2].tags.list.map(item => `<${item}/>`).join(', ')}</label>
+                        <${Section} tag="${tags[0]}" accent="first"/>
+                        <${ResultSection} ..."${tags[1]}" request="${request}" votes="${votes}" feedback="${feedback}" userAcceptCookie="${userAcceptCookie}"/>
+                        <${Section} tag="${tags[2]}" accent="last"/>
+                    </${Tags}>
+                    <${About} show="${!hasTags}">
+                        <${MainSearchForm} ...${form} show="${!hasTags}" specVersion="${specVersion}"/>
+                        <${QuickResults} tagStats="${tagStats}"/>
+                        <div class="about__description">This site helps you understand which tag you can include in another using the <a target="_blank" rel="noopener noreferrer" class="link--no-decoration" href="https://html.spec.whatwg.org"> WHATWG HTML specification </a></div>
+                        <div class="about__description">* This is an alpha version and uses a simple algorithm to test whether one tag can be included in another.</div>
+                    </${About}>
+                </main>
+            </${Main}>`;
     }
 }
 
