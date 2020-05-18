769415c2da1e36e4016407cae3780553173ff2c2
CyberLight <cyberlight@live.ru>
2020-05-18 14:28:16 +0000

    Added wrapper for content of tag section

--- components/ResultSection.js
+++ components/ResultSection.js
@@ -27,27 +27,30 @@ class ResultSection extends Component {
         const votes = props.votes;
         return html`
         <section class="tag__section-result ${this.getSectionClass(props)}">
-            <div class="section-result__container">
-                <div class="section-result__icon ${this.getIconClass(props)}"></div>
-                <div class="section-result__text">${props.text}</div>
-            </div>
-            <div class="reaction-container">
-                <a class="like like--${votes.liked ? 'active' : 'normal'}" href="${props.request.url}&${votes.liked ? 'unlike' : 'like' }"></a>
-                <span>${votes.likes}</span>
-            </div>
-            <div class="reaction-container">
-                <a class="dislike dislike--${votes.disliked ? 'active' : 'normal'}" href="${props.request.url}&${votes.disliked ? 'undislike' : 'dislike'}"></a>
-                <span>${votes.dislikes}</span>
-            </div>
-            ${ props.canAddFeedback && html`
-            <div class="reaction-container">
-                <a class="feedback feedback--${votes.user ? 'active' : 'normal'}" href="${props.request.url}&feedback" title="New feedback"></a>
-                <span>new</span>
-            </div>` 
-            }
-            <div class="reaction-container">
-                <a class="feedbacks feedbacks--${votes.user ? 'active' : 'normal'}" href="${props.request.url}&feedbacks" title="List of feedbacks"></a>
-                <span>list [${props.feedback.count}]</span>
+            <h2 class="section__title">Result of the ability to include a tag in a tag</h2>
+            <div class="tag__section-content">
+                <div class="section-result__container">
+                    <div class="section-result__icon ${this.getIconClass(props)}"></div>
+                    <div class="section-result__text">${props.text}</div>
+                </div>
+                <div class="reaction-container">
+                    <a class="like like--${votes.liked ? 'active' : 'normal'}" href="${props.request.url}&${votes.liked ? 'unlike' : 'like' }"></a>
+                    <span>${votes.likes}</span>
+                </div>
+                <div class="reaction-container">
+                    <a class="dislike dislike--${votes.disliked ? 'active' : 'normal'}" href="${props.request.url}&${votes.disliked ? 'undislike' : 'dislike'}"></a>
+                    <span>${votes.dislikes}</span>
+                </div>
+                ${ props.canAddFeedback && html`
+                <div class="reaction-container">
+                    <a class="feedback feedback--${votes.user ? 'active' : 'normal'}" href="${props.request.url}&feedback" title="New feedback"></a>
+                    <span>new</span>
+                </div>` 
+                }
+                <div class="reaction-container">
+                    <a class="feedbacks feedbacks--${votes.user ? 'active' : 'normal'}" href="${props.request.url}&feedbacks" title="List of feedbacks"></a>
+                    <span>list [${props.feedback.count}]</span>
+                </div>
             </div>
         </section>`;
     }
