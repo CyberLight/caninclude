06322bf1b1a2e700e9120282ebf507c5280fa34f
CyberLight <cyberlight@live.ru>
2020-07-18 06:43:45 +0000

    #12 Added aria-hidden attr to each html element inside ColorText component
    * This attr needed for exclude content from reading by voice assistents

--- components/ColorText.js
+++ components/ColorText.js
@@ -3,7 +3,7 @@ const { html, Component } = require('htm/preact');
 class ColorText extends Component {
     render({ children }) {
         const colors = ['char-red', 'char-green', 'char-yellow'];
-        return children.split('').map((ch, index) => html`<span class="${colors[index % 3]}">${ch}</span>`);
+        return children.split('').map((ch, index) => html`<span aria-hidden="true" class="${colors[index % 3]}">${ch}</span>`);
     }
 }
 
