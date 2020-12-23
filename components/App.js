ca96c56c48227602bf53ee3eae6af47c9b11ee35
CyberLight <cyberlight@live.ru>
2020-12-23 15:41:46 +0000

    #34: Added ribbon with lights for displaying on main page

--- components/App.js
+++ components/App.js
@@ -1,34 +1,48 @@
 const { html, Component } = require('htm/preact');
-const SearchForm = require('./SearchForm');
 const MainSearchForm = require('./MainSearchForm');
 const Tags = require('./Tags');
 const Section = require('./Section');
 const ResultSection = require('./ResultSection');
-const Footer = require('./Footer');
 const Tips = require('./Tips');
 const About = require('./About');
 const QuickResults = require('./QuickResults');
 const CookieConsent = require('./CookieConsent');
 const NewFeedback = require('./NewFeedback');
 const TagFeedbacks = require('./TagFeedbacks');
+const Ribbon = require('./Ribbon');
 const Main = require('./Main');
 
 class App extends Component {
-    render({ form, tags, tips = [], tagStats, request, specVersion, votes, userAcceptCookie, showFeedback, showFeedbacks, feedback, feedbacks, canAddFeedback }) {
-        const hasTags = tags.length > 0;
-
-        return html`
+  // eslint-disable-next-line class-methods-use-this
+  render({
+    form,
+    tags,
+    tips = [],
+    tagStats,
+    request,
+    specVersion,
+    votes,
+    userAcceptCookie,
+    showFeedback,
+    showFeedbacks,
+    feedback,
+    feedbacks,
+    canAddFeedback,
+  }) {
+    const hasTags = tags.length > 0;
+    return html`
             <${Main} form="${form}" tags="${tags}" request="${request}">
-                ${ !userAcceptCookie && html`<${CookieConsent}/>` }
+                ${!userAcceptCookie && html`<${CookieConsent}/>`}
+                ${!hasTags && html`<${Ribbon} />`}
                 <main class="content">
                     <${Tips} tips="${tips}"/>
                     <${NewFeedback} request="${request}" form="${form}" show="${showFeedback}"/>
                     <${TagFeedbacks} request="${request}" feedbacks="${feedbacks}" show="${showFeedbacks}" />
                     <${Tags} show="${hasTags}">
                         <input checked id="first" class="tab__option" type="radio" name="tabs" value="first"/>
-                        <label class="tab__name" for="first">${tags.length && tags[0].tags.list.map(item => `<${item}/>`).join(', ')}</label>
+                        <label class="tab__name" for="first">${tags.length && tags[0].tags.list.map((item) => `<${item}/>`).join(', ')}</label>
                         <input id="second" class="tab__option" type="radio" name="tabs" value="second"/>
-                        <label class="tab__name" for="second">${tags.length && tags[2].tags.list.map(item => `<${item}/>`).join(', ')}</label>
+                        <label class="tab__name" for="second">${tags.length && tags[2].tags.list.map((item) => `<${item}/>`).join(', ')}</label>
                         <${Section} tag="${tags[0]}" accent="first"/>
                         <${ResultSection} ..."${tags[1]}" request="${request}" votes="${votes}" feedback="${feedback}" userAcceptCookie="${userAcceptCookie}" canAddFeedback="${canAddFeedback}"/>
                         <${Section} tag="${tags[2]}" accent="last"/>
@@ -41,7 +55,7 @@ class App extends Component {
                     </${About}>
                 </main>
             </${Main}>`;
-    }
+  }
 }
 
 module.exports = App;
