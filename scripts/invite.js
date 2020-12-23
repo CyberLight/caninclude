cebbcf0fa77418e8afb8d0875c1cc34babc1d28f
CyberLight <cyberlight@live.ru>
2020-12-23 06:47:51 +0000

    #32: Cleanup invite script

--- scripts/invite.js
+++ scripts/invite.js
@@ -1,22 +1,21 @@
 const util = require('util');
 const md5 = require('md5');
-const sqlite3 = require("sqlite3").verbose();
+const sqlite3 = require('sqlite3').verbose();
 const { v4: uuidv4 } = require('uuid');
 
+(async function run() {
+  const options = process.argv.slice(2);
+  if (!options.length) {
+    throw new Error('No parameters');
+  }
 
-
-!async function run() {
-    const options = process.argv.slice(2);
-    if (!options.length) {
-        throw new Error('No parameters');
-    }
-
-    const dbConn = new sqlite3.Database("./.data/sqlite.db");
-    const runAsync = util.promisify(dbConn.run).bind(dbConn);
-    const closeAsync = util.promisify(dbConn.close).bind(dbConn);
-    const argsDict = Object.fromEntries(options.map(v => v.split('=')));
-    const key = md5(`${uuidv4()}${argsDict.role}`);
-    await runAsync(`INSERT INTO invites(key, role) VALUES(?,?)`, [key, argsDict.role]);
-    console.log(`/invites/${key}/apply`);
-    await closeAsync();
-}();
+  const dbConn = new sqlite3.Database('./.data/sqlite.db');
+  const runAsync = util.promisify(dbConn.run).bind(dbConn);
+  const closeAsync = util.promisify(dbConn.close).bind(dbConn);
+  const argsDict = Object.fromEntries(options.map((v) => v.split('=')));
+  const key = md5(`${uuidv4()}${argsDict.role}`);
+  await runAsync('INSERT INTO invites(key, role) VALUES(?,?)', [key, argsDict.role]);
+  // eslint-disable-next-line no-console
+  console.log(`/invites/${key}/apply`);
+  await closeAsync();
+}());
