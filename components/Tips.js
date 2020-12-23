562d5902ce700bf96722a298941a2ec981feb276
CyberLight <cyberlight@live.ru>
2020-12-23 05:53:59 +0000

    #30: Cleanup and improve rendering of tag section information

--- components/Tips.js
+++ components/Tips.js
@@ -1,14 +1,15 @@
 const { html, Component } = require('htm/preact');
 
 class Tips extends Component {
-    render({ tips = [] }) {
-        return tips && tips.length && tips.map(tip => html`
-           <div class="tip tip--${tip.type}"> 
-                <ul class="tip__item">
-                    ${tip.messages.map(message => html`<li dangerouslySetInnerHTML="${{ __html: message }}"/>`)}
+  // eslint-disable-next-line class-methods-use-this
+  render({ tips = [] }) {
+    return (tips && tips.length && tips.map((tip) => html`
+            <div class="tip tip--${tip.type}">
+                <ul class="tip__item tip__item--${tip.type}">
+                    ${tip.messages.map((message) => html`<li dangerouslySetInnerHTML="${{ __html: message }}"/>`)}
                 </ul>
-           </div>`) || null;
-    }
+            </div>`)) || null;
+  }
 }
 
 module.exports = Tips;
