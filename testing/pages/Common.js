a8e9432739be1996099372ac68df833f3311b6f2
CyberLight <cyberlight@live.ru>
2020-12-28 11:25:01 +0000

    #44: Added common locators for cookie consent dialog

--- testing/pages/Common.js
+++ testing/pages/Common.js
@@ -1,10 +1,20 @@
 // const { I } = inject();
 
+const CookieConsentDialogLocator = locate('div[class="cookie-consent"]');
+
 module.exports = {
   helpers: {
     detailUrl(item) {
       return `/can/include/?child=${item.child}&parent=${item.parent}`;
     },
+  },
+  buttons: {
+    acceptCookieContent: locate('a[class="cookie-consent__accept-button"]').inside(CookieConsentDialogLocator),
+  },
+  dialogs: {
+    cookieConsent: CookieConsentDialogLocator.withChild(
+      locate('a[class="cookie-consent__accept-button"]').withText('Accept'),
+    ),
   },
   // insert your locators and methods here
 };
