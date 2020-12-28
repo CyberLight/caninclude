380d83b3818e0da361f6da61fdd964a4f14d7210
CyberLight <cyberlight@live.ru>
2020-12-28 11:23:45 +0000

    #44: Updated steps.d.ts files using "npm run cc:def" command

--- steps.d.ts
+++ steps.d.ts
@@ -1,11 +1,12 @@
 /// <reference types='codeceptjs' />
 type steps_file = typeof import('./testing/steps/steps_file.js');
 type MainPage = typeof import('./testing/pages/Main.js');
+type CommonPage = typeof import('./testing/pages/Common.js');
 type DbHelper = import('./testing/helpers/dbHelper.js');
 type ResembleHelper = import('codeceptjs-resemblehelper');
 
 declare namespace CodeceptJS {
-  interface SupportObject { I: I, current: any, MainPage: MainPage }
+  interface SupportObject { I: I, current: any, MainPage: MainPage, CommonPage: CommonPage }
   interface Methods extends Puppeteer, DbHelper, ResembleHelper {}
   interface I extends ReturnType<steps_file>, WithTranslation<DbHelper>, WithTranslation<ResembleHelper> {}
   namespace Translation {
